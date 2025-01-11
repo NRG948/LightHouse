68dec564a1413c909fd1091d63ab8c09ed484998
Colin <hungrymamamia@gmail.com>
2025-01-11 02:29:47 -0800

    Add PageView

--- lib/pages/data_entry_paged.dart
+++ lib/pages/data_entry_paged.dart
@@ -1,48 +1,72 @@
 import "dart:convert";
 
 import "package:flutter/material.dart";
+import "package:flutter/rendering.dart";
 import "package:lighthouse/constants.dart";
 import "package:lighthouse/filemgr.dart";
 import "package:lighthouse/layouts.dart";
-import "package:lighthouse/pages/data_entry.dart";
 import "package:lighthouse/pages/entry_widgets.dart";
 
 class DataEntryPaged extends StatelessWidget {
+
+  List<Widget> createWidgetList(List<Map<String,String>> widgets) {
+    final widgetList = widgets.map((widgetData) {
+      final title = widgetData["title"]!;
+      final type = widgetData["type"]!;
+      final jsonKey = widgetData["jsonKey"]!;
+      exportData[jsonKey] = "0"; // creates entry in global data object
+      switch(type) {
+        case "spinbox":
+          return NRGSpinbox(title: title,jsonKey: jsonKey,);
+        case "textbox":
+          return NRGTextBox(title: title,jsonKey: jsonKey,);
+        case "checkbox":
+          return NRGCheckbox(title: title, jsonKey: jsonKey,);
+        case "numberbox":
+          return NRGTextBox(title: title, jsonKey: jsonKey, numeric: true,);
+        case "dropdown":
+          if (!(widgetData.containsKey("options"))) { return Text("Widget $title doesn't have dropdown options specified.");}
+          final options = widgetData["options"]!.split(",");
+          return NRGDropdown(title: title, jsonKey: jsonKey,options: options,);}
+        return Text("type $type isn't a valid type");
+    }).toList();
+    return widgetList;
+    
+  }
+  
+  List<Widget> createWidgetPages(List<Map<String,dynamic>> pages) {
+    exportData.clear();
+    return pages.map((page) {
+      final pageTitle = page["title"];
+      final widgetList = createWidgetList(page["widgets"]);
+      return Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top:10.0, left: 10.0, right:10.0),
+            child: ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: widgetList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top:10.0),
+                  child: widgetList[index],
+                );
+              },
+            ),
+          ),
+        );
+    }).toList();
+  }
+
   const DataEntryPaged({super.key});
   static final Map<String, String> exportData = {};
    @override
    Widget build(BuildContext context) {
    final String activeConfig = (ModalRoute.of(context)?.settings.arguments as String?)!;
    final layoutJSON = layoutMap.containsKey(activeConfig) ? layoutMap[activeConfig]! : Map();
-   if (layoutJson.)
-  //   final String activeConfig = (ModalRoute.of(context)?.settings.arguments as String?)!;
-  //   final layoutJSON = layoutMap.containsKey(activeConfig) ? layoutMap[activeConfig]! : Map();
-  //   if (layoutJSON.containsKey("widgets")) {Navigator.pushNamed(context, "/entry", arguments: activeConfig);}
-  //   final widgetList = (layoutJSON["widgets"] as List).map((widgetData) {
-  //     final title = widgetData["title"];
-  //     final type = widgetData["type"];
-  //     final jsonKey = widgetData["jsonKey"];
+   final controller = PageController(
+    initialPage: 0,
 
-  //     if (type == "spinbox") {return NRGSpinbox(title: title,jsonKey: jsonKey,);}
-  //     if (type == "textbox") {return NRGTextBox(title: title, jsonKey: jsonKey,);}
-  //     if (type == "textboxLarge") {return NRGTextBox(title: title, jsonKey: jsonKey, height: "200");}
-  //     if (type == "checkbox") {return NRGCheckbox(title: title, jsonKey: jsonKey);}
-  //     if (type == "numberbox") {return NRGTextBox(title: title, jsonKey: jsonKey, numeric: true);}
-  //     if (type == "dropdown") {
-  //       if (!(widgetData.containsKey("options"))) { return Text("Widget $title doesn't have dropdown options specified.");}
-  //       final options = widgetData["options"].split(",");
-  //       return NRGDropdown(title: title, jsonKey: jsonKey,options: options,);}
-  //     return Text("type $type isn't a valid type you idiot");
-  //   }).toList();
-  //   widgetList.add(SaveJsonButton());
-
-    // exportData.clear();
-    // for (var widgetData in layoutJSON["widgets"]) {
-    //   final key = widgetData["jsonKey"];
-    //   exportData[key] = "0";
-    // }
-
-
+   );
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Constants().pastelRed,
@@ -55,22 +79,10 @@ class DataEntryPaged extends StatelessWidget {
         leading: IconButton(onPressed: () {Navigator.pushNamed(context, "/home");}, icon: Icon(Icons.home)),
       ),
       body: PageView(
-        children: 
+        controller: controller,
+        scrollDirection: Axis.horizontal,
+        children: createWidgetPages(layoutJSON["pages"])
         
-         Center(
-          child: Padding(
-            padding: const EdgeInsets.only(top:10.0, left: 10.0, right:10.0),
-            child: ListView.builder(
-              itemCount: widgetList.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(top:10.0),
-                  child: widgetList[index],
-                );
-              },
-            ),
-          ),
-        ),
       )
       
     );
