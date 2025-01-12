8bf52959d0535257895a63ad0197604d0c39ca75
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2025-01-12 11:19:05 -0800

    Update stopwatch.dart

--- lib/widgets/stopwatch.dart
+++ lib/widgets/stopwatch.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class NRGStopwatch extends StatefulWidget {
   NRGStopwatch({super.key});
 
+  final String title = "Stopwatch";
+  final String height = "100";
+  final String width = "400";
+
   final stopwatch = Stopwatch();
 
   @override
@@ -11,6 +15,8 @@ class NRGStopwatch extends StatefulWidget {
 
 class _NRGCheckboxState extends State<NRGStopwatch> {
   Stopwatch get _stopwatch => widget.stopwatch;
+  String get _height => widget.height;
+  String get _width => widget.width;
 
   @override
   void initState() {
@@ -21,7 +27,7 @@ class _NRGCheckboxState extends State<NRGStopwatch> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      height: ,
     );
   }
 }
