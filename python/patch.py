f58bd2a2cd8b906c678e5e42278267eba81b8dbe
Colin <62516283+faletto@users.noreply.github.com>
2025-03-15 11:33:50 -0700

    Create patch script

---
+++ python/patch.py
@@ -0,0 +1,22 @@
+import requests
+import json
+data = {}
+
+
+mt_shortcuts = {
+    "q": "Qualifications",
+    "f": "Finals",
+    "p": "Playoffs"
+}
+
+print("Welcome to LightHouse data patcher")
+data["dataType"] = "Atlas"
+data["matchNumber"] = int(input("enter match number:"))
+data["teamNumber"] = int(input("enter team number:"))
+data["matchType"] = mt_shortcuts[input("Enter match type (q for quals, f for finals, p for playoffs):")]
+data["driverStation"] = input("enter driver station (Format like 'Red 1'):")
+data["patcher"] = input("Enter scouter name:")
+data["comments"] = input("Enter comments:")
+
+a = requests.post("https://containers.tail53c89.ts.net/api/patch",json.dumps(data),headers={"Content-Type": "application/json"})
+print(a.content)
