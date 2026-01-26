603a113aa55bc7ee8cf5f58042bd13d8c38b4bec
raptorhawk9 <142059090+raptorhawk9@users.noreply.github.com>
2026-01-25 16:49:50 -0800

    add auto path selector to atlas

--- lib/pages/rebuilt/atlas.dart
+++ lib/pages/rebuilt/atlas.dart
@@ -4,6 +4,7 @@ import 'package:lighthouse/pages/data_entry_page.dart';
 import 'package:lighthouse/pages/data_entry_sub_page.dart';
 import 'package:lighthouse/widgets/game_agnostic/match_info.dart';
 import 'package:lighthouse/widgets/game_agnostic/placeholder.dart';
+import 'package:lighthouse/widgets/rebuilt/auto_path_selector.dart';
 
 class Atlas extends StatefulWidget {
   const Atlas({super.key});
@@ -15,30 +16,40 @@ class Atlas extends StatefulWidget {
 class AtlasState extends State<Atlas> {
   @override
   Widget build(BuildContext context) {
-    return DataEntryPage(
-      name: "Atlas", 
-      pages: {
-        "Setup": DataEntrySubPage(icon: CustomIcons.wrench, content: Container(
-          child: Column(
-            children: [
-              MatchInfo()
-            ],
-          )
-        )), 
-        "Auto": DataEntrySubPage(icon: CustomIcons.autonomous,content: Container(
-          child: NRGPlaceholder(title: "auto", jsonKey: "auto", height: 20, width: 20)
-        ),), 
-        // TODO: get more sensible icons
-        "Onshift": DataEntrySubPage(icon: CustomIcons.racecar, content: Container(
-           child: NRGPlaceholder(title: "onshift", jsonKey: "onshift", height: 10, width: 20),
-        ),), 
-        "Offshift": DataEntrySubPage(icon: CustomIcons.timer, content: Container(
-           child: NRGPlaceholder(title: "offshift", jsonKey: "offshift", height: 10, width: 20),
-        ),),
-        "Endgame": DataEntrySubPage(icon: CustomIcons.flag, content: Container(
-           child: NRGPlaceholder(title: "endgame", jsonKey: "endgame", height: 10, width: 20),
-        ),)
-      }
-    );
+    return DataEntryPage(name: "Atlas", pages: {
+      "Setup": DataEntrySubPage(
+          icon: CustomIcons.wrench,
+          content: Container(
+              child: Column(
+            children: [MatchInfo()],
+          ))),
+      "Auto": DataEntrySubPage(
+        icon: CustomIcons.autonomous,
+        content:
+            Container(margin: EdgeInsets.all(5), child: AutoPathSelector()),
+      ),
+      // TODO: get more sensible icons
+      "Onshift": DataEntrySubPage(
+        icon: CustomIcons.racecar,
+        content: Container(
+          child: NRGPlaceholder(
+              title: "onshift", jsonKey: "onshift", height: 10, width: 20),
+        ),
+      ),
+      "Offshift": DataEntrySubPage(
+        icon: CustomIcons.timer,
+        content: Container(
+          child: NRGPlaceholder(
+              title: "offshift", jsonKey: "offshift", height: 10, width: 20),
+        ),
+      ),
+      "Endgame": DataEntrySubPage(
+        icon: CustomIcons.flag,
+        content: Container(
+          child: NRGPlaceholder(
+              title: "endgame", jsonKey: "endgame", height: 10, width: 20),
+        ),
+      )
+    });
   }
 }
