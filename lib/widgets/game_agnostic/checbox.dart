4a0e04cf20de9671bacde0a41f72169515eaa552
GuestUser1000101 <ryanzou2008@gmail.com>
2026-02-14 14:09:13 -0800

    Add locking to checkbox

--- lib/widgets/game_agnostic/checbox.dart
+++ lib/widgets/game_agnostic/checbox.dart
@@ -24,6 +24,12 @@ class CustomCheckbox extends StatefulWidget {
   /// The color of the checkbox.
   final Color optionColor;
 
+  /// The color of the checkbox when [isLocked] is ```true```.
+  final Color lockedColor;
+
+  /// If the checkbox stops accepting inputs.
+  final bool isLocked;
+
   /// Creates a checkbox that registers clicks on the checkbox and the title text.
   const CustomCheckbox(
       {super.key,
@@ -32,7 +38,9 @@ class CustomCheckbox extends StatefulWidget {
       this.initialValue = false,
       this.selectColor = Colors.black,
       this.textColor = Colors.black,
-      this.optionColor = Colors.white});
+      this.optionColor = Colors.white,
+      this.lockedColor = Colors.grey,
+      this.isLocked = false,});
 
   static void _noop(bool value) {}
 
@@ -51,7 +59,9 @@ class _CustomCheckboxState extends State<CustomCheckbox> {
   Color get _selectColor => widget.selectColor;
   Color get _optionColor => widget.optionColor;
   Color get _textColor => widget.textColor;
+  Color get _lockedColor => widget.lockedColor;
 
+  bool get _isLocked => widget.isLocked;
   bool currentValue = false;
 
   @override
@@ -66,49 +76,52 @@ class _CustomCheckboxState extends State<CustomCheckbox> {
       builder: (context, constraints) {
         _width = constraints.maxWidth;
         _height = constraints.maxHeight;
-        return GestureDetector(
-          onTapUp: (details) {
-            setState(() {
-              currentValue = !currentValue;
-              HapticFeedback.mediumImpact();
-            });
-            _onToggle(currentValue);
-          },
-          child: Container(
-            width: _width,
-            height: _height,
-            color: Color.fromARGB(1, 255, 255, 255),
-            child: Row(
-              spacing: _height * 0.3,
-              children: [
-                Container(
-                  width: _height * 0.6,
-                  height: _height * 0.6,
-                  decoration: BoxDecoration(
-                    color: _optionColor,
-                    borderRadius: BorderRadius.circular(_height * 0.2),
+        return AbsorbPointer(
+          absorbing: _isLocked,
+          child: GestureDetector(
+            onTapUp: (details) {
+              setState(() {
+                currentValue = !currentValue;
+                HapticFeedback.mediumImpact();
+              });
+              _onToggle(currentValue);
+            },
+            child: Container(
+              width: _width,
+              height: _height,
+              color: Color.fromARGB(1, 255, 255, 255),
+              child: Row(
+                spacing: _height * 0.3,
+                children: [
+                  Container(
+                    width: _height * 0.6,
+                    height: _height * 0.6,
+                    decoration: BoxDecoration(
+                      color: _isLocked ? _lockedColor : _optionColor,
+                      borderRadius: BorderRadius.circular(_height * 0.2),
+                    ),
+                    child: FractionallySizedBox(
+                        widthFactor: 0.5,
+                        heightFactor: 0.5,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color:
+                                currentValue ? _selectColor : Colors.transparent,
+                            borderRadius: BorderRadius.circular(_height * 0.12),
+                          ),
+                        )),
                   ),
-                  child: FractionallySizedBox(
-                      widthFactor: 0.5,
-                      heightFactor: 0.5,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color:
-                              currentValue ? _selectColor : Colors.transparent,
-                          borderRadius: BorderRadius.circular(_height * 0.12),
-                        ),
-                      )),
-                ),
-                SizedBox(
-                  width: _width - _height,
-                  child: AutoSizeText(
-                    _title,
-                    style: comfortaaBold(_height * 0.7, color: _textColor),
-                    maxLines: 1,
-                    minFontSize: 9,
-                  ),
-                )
-              ],
+                  SizedBox(
+                    width: _width - _height,
+                    child: AutoSizeText(
+                      _title,
+                      style: comfortaaBold(_height * 0.7, color: _isLocked ? Colors.black.withAlpha(100) : _textColor),
+                      maxLines: 1,
+                      minFontSize: 9,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         );
