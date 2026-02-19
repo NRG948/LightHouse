189ec2d8add68bce98c1595a3f17acb2ea86def9
GuestUser1000101 <ryanzou2008@gmail.com>
2026-02-18 17:36:47 -0800

    Add shift container

--- lib/pages/rebuilt/atlas.dart
+++ lib/pages/rebuilt/atlas.dart
@@ -6,10 +6,10 @@ import 'package:lighthouse/pages/data_entry_sub_page.dart';
 import 'package:lighthouse/widgets/game_agnostic/checbox.dart';
 import 'package:lighthouse/widgets/game_agnostic/match_info.dart';
 import 'package:lighthouse/widgets/game_agnostic/placeholder.dart';
-import 'package:lighthouse/widgets/game_agnostic/auto_path_selector.dart';
 import 'package:lighthouse/widgets/rebuilt/cycle_counter.dart';
 import 'package:lighthouse/widgets/rebuilt/rebuilt_auto_path_selector.dart';
 import 'package:lighthouse/widgets/rebuilt/rebuilt_endgame_tag_selector.dart';
+import 'package:lighthouse/widgets/rebuilt/shift_container.dart';
 import 'package:lighthouse/widgets/rebuilt/tower_location_selector.dart';
 
 class Atlas extends StatefulWidget {
@@ -59,11 +59,52 @@ class AtlasState extends State<Atlas> {
         icon: CustomIcons.racecar,
         content: Container(
           margin: EdgeInsets.all(15),
-          child: Column(
-            spacing: margin,
-            children: [
-              CycleCounter(),
-            ],
+          child: ShiftContainer(
+            height: 75,
+            margin: margin,
+            shifts: ["Transition", "1st Onshift", "2nd Onshift"],
+            childBuilder: (index) => Column(
+              spacing: margin,
+              children: [
+                CycleCounter(),
+                Container(
+                  height: 150,
+                  padding: EdgeInsets.all(margin),
+                  decoration: BoxDecoration(
+                      color: Constants.pastelWhite,
+                      borderRadius: BorderRadius.circular(margin)),
+                  child: Column(
+                    spacing: margin,
+                    children: [
+                      Expanded(
+                        child: CustomCheckbox(
+                          title: "Feeding",
+                          selectColor: Constants.pastelWhite,
+                          optionColor: Constants.pastelYellow,
+                          textColor: Constants.pastelBrown,
+                        ),
+                      ),
+                      Expanded(
+                        child: CustomCheckbox(
+                          title: "Disabled",
+                          selectColor: Constants.pastelWhite,
+                          optionColor: Constants.pastelRed,
+                          textColor: Constants.pastelBrown,
+                        ),
+                      ),
+                      Expanded(
+                        child: CustomCheckbox(
+                          title: "Defending",
+                          selectColor: Constants.pastelWhite,
+                          optionColor: Constants.pastelYellow,
+                          textColor: Constants.pastelBrown,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/rebuilt/shift_container.dart
@@ -0,0 +1,96 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:lighthouse/constants.dart';
+
+class ShiftContainer extends StatefulWidget {
+  final double height;
+  final double? margin;
+  final List<String> shifts;
+  final Widget Function(int index) childBuilder;
+
+  const ShiftContainer({
+    super.key,
+    required this.height,
+    this.margin,
+    required this.shifts,
+    required this.childBuilder,
+  });
+
+  @override
+  State<ShiftContainer> createState() => _ShiftContainerState();
+}
+
+class _ShiftContainerState extends State<ShiftContainer>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+  double get _height => widget.height;
+  double get _fontSize => 15;
+  double get _margin => widget.margin ?? _height / 8;
+  Widget Function(int index) get _childBuilder => widget.childBuilder;
+  List<String> get _shifts => widget.shifts;
+
+  int currentIndex = 0;
+  late final List<Widget> _children;
+
+  @override
+  void initState() {
+    super.initState();
+    _children = List.generate(_shifts.length,
+        (int x) => KeyedSubtree(key: ValueKey(x), child: _childBuilder(x)));
+  }
+
+  Widget buildSelector(int index) {
+    return Expanded(
+      child: GestureDetector(
+        child: Container(
+          color: Color.fromARGB(1, 255, 255, 255),
+          child: Center(
+            child: Container(
+              padding: EdgeInsets.all(_margin / 2),
+              decoration: BoxDecoration(
+                  color: index == currentIndex ? Constants.pastelYellow : Color.fromARGB(1, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(_margin / 2)),
+              child: AutoSizeText(
+                _shifts[index],
+                style: comfortaaBold(_fontSize,
+                    color: index == currentIndex ? Constants.pastelWhite : Constants.pastelBrown,
+                    customFontWeight: index == currentIndex ? FontWeight.w900 : FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+        onTapUp: (details) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: _margin,
+      children: [
+        Center(
+            child: Container(
+          height: _height,
+          decoration: BoxDecoration(
+            color: Constants.pastelWhite,
+            borderRadius: BorderRadius.circular(_margin),
+          ),
+          child: Row(
+            children:
+                List.generate(_shifts.length, (int x) => buildSelector(x)),
+          ),
+        )),
+        IndexedStack(
+          index: currentIndex,
+          children: _children,
+        ),
+      ],
+    );
+  }
+}
